3d82c325128579b89c806d53a4dd99c09d8e368a
1IKOTI1 <matreq12@gmai.com>
2026-02-20 18:46:40 +0400

    исправление 12

--- app.py
+++ app.py
@@ -63,3 +63,8 @@ def  get_winners():
 if __name__=='__main__':
     app.run(debug=True)
 
+import os  # ← ЭТОТ ИМПОРТ ДОЛЖЕН БЫТЬ ВВЕРХУ ФАЙЛА!
+
+if __name__ == '__main__':
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)  # ← ВАЖНО: host="0.0.0.0"
