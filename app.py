ba6a263a4ce0bfbf07c4cbf8a311c996db34c1c9
1IKOTI1 <matreq12@gmai.com>
2026-02-20 18:13:08 +0400

    временый коммит

--- app.py
+++ app.py
@@ -62,3 +62,7 @@ def  get_winners():
     
 if __name__=='__main__':
     app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
